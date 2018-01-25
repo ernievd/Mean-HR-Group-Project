e4bc1d950decf16466dcd90c76d7506225f03c8d
Andrew Johnson <ajj@byu.net>
2018-01-25 16:42:26 -0600

    Added client-side routing

--- server/public/scripts/clients.js
+++ server/public/scripts/clients.js
@@ -1,1 +1,25 @@
-var hrApp = angular.module('App', []);
+var hrApp = angular.module('App', ['ngRoute']);
+
+// app configuration
+hrApp.config(function($routeProvider) {
+    console.log('config loaded');
+    // define our client side routes
+    $routeProvider
+        .when('/add', {
+            templateUrl: '/views/add.controller.html',
+            controller: 'AddController as vm'
+        })
+        .when('/track', {
+            templateUrl: '/views/track.controller.html',
+            controller: 'TrackController as vm'
+        })
+        .when('/reports', {
+            templateUrl: '/views/reports.controller.html',
+            controller: 'ReportsController as vm'
+        })
+        .otherwise(
+            { redirectTo: '/add' }
+        );
+    
+});
+
