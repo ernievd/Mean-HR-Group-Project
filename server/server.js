8131ca755706a420d4120f61a706cc6e44543ed5
ernievd <ernievd@gmail.com>
2018-01-25 14:27:03 -0600

    added server file

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+// const ***** = require('./routes/*****');
+
+/** ---------- MIDDLEWARE ---------- **/
+app.use(express.static('server/public/'));
+app.use(bodyParser.json()); // needed for angular requests
+
+/** ---------- EXPRESS ROUTES ---------- **/
+// app.use('/games', gameRouter);
+
+/** ---------- MONGOOSE ------------ **/
+const mongoose = require('mongoose');
+
+// 27017 is the default mongo port number
+const databaseUrl = 'mongodb://localhost:27017/gamestop';
+
+// connect to mongoDB
+mongoose.connect(databaseUrl);
+
+// optional output from connectione events
+mongoose.connection.on('connected', () => {
+	console.log('mongoose is connected');
+});
+
+mongoose.connection.on('error', () => {
+	console.log('mongoose connection failed');
+});
+
+
+/** ---------- START SERVER ---------- **/
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function() {
+	console.log('Listening on port: ', app.get('port'));
+});
