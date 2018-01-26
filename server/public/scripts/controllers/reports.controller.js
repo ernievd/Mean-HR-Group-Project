fce60c6cf91912fad4363cb0ff0107b14fce0beb
Andrew Johnson <ajj@byu.net>
2018-01-26 13:37:45 -0600

    Counting employees appears on DOM

--- server/public/scripts/controllers/reports.controller.js
+++ server/public/scripts/controllers/reports.controller.js
@@ -1,17 +1,27 @@
 hrApp.controller('ReportsController', [ '$http', function($http) {
 	const self = this;
 
-self.getAverageSalary = function(){
-	$http.get('/reports')
-		.then (function (response) {
-			console.log('Received report average salary data response -', response);
-			self.employeeData = response.data;
-		})
-		.catch (function (response) {
-			console.log('Received report average salary error on get request')
-		})
-}
-
+	self.getAverageSalary = function(){
+		$http.get('/reports')
+			.then (function (response) {
+				console.log('Received report average salary data response -', response);
+				self.employeeData = response.data;
+			})
+			.catch (function (response) {
+				console.log('Received report average salary error on get request')
+			})
+	}
+	self.getEmployeeCount = function() {
+		$http.get('reports/count')
+			.then (function (response) {
+				console.log('received employee count', response.data.count);
+				self.count = response.data.count;
+			})
+			.catch (function (response) {
+				console.log('error in employee count');
+			});
+	}
+	self.getEmployeeCount();
 
 	// self.getAverageSalary = function(){
 	// 	$http.get('/reports/average')
