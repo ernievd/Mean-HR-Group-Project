0461e925ec763f4cda88877ac7b8fc6ec48139dc
Andrew Johnson <ajj@byu.net>
2018-01-26 11:30:13 -0600

    Connects to Mongo, has dummy data route

--- server/routes/reports.router.js
+++ server/routes/reports.router.js
@@ -17,10 +17,50 @@ let employeeSchema = new mongoose.Schema(
 // mongoose.model( name of the collection, schema definition)
 let Employee = mongoose.model('Employee', employeeSchema);
 
-// GET route
+// dummy data GET route
 
+router.get('/dummydata', (req, res) => {
+	console.log('hit reports GET route');
+
+    // create an instnace of our model
+    let employeeToAdd = new Employee({
+		name: 'Andrew Johnson',
+		idNumber: 134,
+		salary: 100000,
+		title: 'Bossman',
+		hireDate: '2017-01-04'
+	});
+
+    // create a new Document in our database and collection
+    employeeToAdd.save( (error, savedEmployee) => {
+        if (error) {
+            console.log('error on save: ', error);
+            res.sendStatus(500);
+        } else {
+            console.log('new employee Document: ', savedEmployee);
+            res.sendStatus(201);
+        }
+    });
+
+});
+
+// GET route
+router.get('/', (req, res) => {
+	console.log('hit reports GET route');
+
+	Employee.find({}, (error, foundEmployees) => {
+        if (error) {
+            console.log('error on save: ', error);
+            res.sendStatus(500);
+        } else {
+            console.log('new Employee Document: ', foundEmployees);
+            res.send(foundEmployees);
+        }
+	});
+});
 
 // POST route
+
 
 
 // DELETE route
